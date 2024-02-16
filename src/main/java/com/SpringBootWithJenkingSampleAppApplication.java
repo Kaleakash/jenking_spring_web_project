69da447ca2fcc93e5cf20cafeb99b69cf3be58e9
akash <akash300383@gmail.com>
2024-02-16 15:05:51 +0530

    remove compile time error

--- src/main/java/com/SpringBootWithJenkingSampleAppApplication.java
+++ src/main/java/com/SpringBootWithJenkingSampleAppApplication.java
@@ -9,6 +9,6 @@ public class SpringBootWithJenkingSampleAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithJenkingSampleAppApplication.class, args);
 		System.out.println("spring boot up on port number 9090");
-	
+	}
 
 }
